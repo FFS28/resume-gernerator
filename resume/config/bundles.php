11443a3152e3f20bc44f19f04f37fbc51a0c79e1
Jérémy ACHAIN <achain.jeremy@gmail.com>
2020-01-15 18:10:51 +0100

    Add new spam bundle

--- resume/config/bundles.php
+++ resume/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
 ];
--- resume/src/Form/Type/ContactFormType.php
+++ resume/src/Form/Type/ContactFormType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form\Type;
 
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,14 +18,12 @@ class ContactFormType extends AbstractType
             ->add('email', EmailType::class, ['required' => true])
             ->add('message', TextareaType::class, ['required' => true])
             ->add('send', SubmitType::class)
+            ->add('captcha', CaptchaType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'timed_spam' => true,
-            'honeypot' => true,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
