965b7ac14f330cfd6d3b2054b040f6db0e0efa86
Jérémy ACHAIN <achain.jeremy@gmail.com>
2020-01-30 15:48:02 +0100

    Improve dareboost

---
+++ resume/src/Subscriber/ResponseSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class ResponseSubscriber
+ * @package AppBundle\Subscriber
+ */
+class ResponseSubscriber implements EventSubscriberInterface
+{
+    /** @inheritdoc */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onResponse'
+        ];
+    }
+
+    /**
+     * Callback function for event subscriber
+     * @param ResponseEvent $event
+     */
+    public function onResponse(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+
+        $response->headers->set("Content-Security-Policy", "default-src 'self' 'unsafe-inline';"
+            . "script-src 'self' 'unsafe-inline'");
+        $response->headers->set("X-Frame-Options", 'deny');
+        $response->headers->set("X-XSS-Protection", '1; mode=block');
+        $response->headers->set("X-Content-Type-Options", 'nosniff');
+    }
+}
