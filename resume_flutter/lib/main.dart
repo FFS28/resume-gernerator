a91856faabe50b24feed1a9648389dadd5c5831f
Jérémy ACHAIN <achain.jeremy@gmail.com>
2020-03-02 18:22:36 +0100

    Update flutter project

--- resume_flutter/lib/main.dart
+++ resume_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resume_flutter/services/api.dart';
 
 void main() => runApp(MyApp());
 
@@ -54,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      Api.login('test', 'test');
     });
   }
 
