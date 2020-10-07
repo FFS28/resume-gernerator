878674ab664878e2e0e9a368d5c92097b03e093e
Jérémy ACHAIN <achain.jeremy@gmail.com>
2020-10-07 10:42:15 +0200

    Update & improve invoice list

--- resume_php/config/bundles.php
+++ resume_php/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
