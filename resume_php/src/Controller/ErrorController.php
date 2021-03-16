c3c6c96b5a19201c1c07bfacf8186f7846e4acdc
Jérémy ACHAIN <achain.jeremy@gmail.com>
2021-03-16 15:00:23 +0100

    Fix prod error page

---
+++ resume_php/src/Controller/ErrorController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+
+class ErrorController extends AbstractController
+{
+    public function show(FlattenException $exception, DebugLoggerInterface $logger = null)
+    {
+        return $this->render('bundles/TwigBundle/Exception/error.html.twig', [
+            "code" => $exception->getStatusCode(),
+            "message" =>$exception->getStatusText()
+        ]);
+    }
+}
