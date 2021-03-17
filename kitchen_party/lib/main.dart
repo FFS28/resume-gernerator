0fdf955ad0ae4673df7663136393f7f897244c9d
Jérémy ACHAIN <achain.jeremy@gmail.com>
2021-03-17 11:25:13 +0100

    Fix

--- kitchen_party/lib/main.dart
+++ kitchen_party/lib/main.dart
@@ -56,7 +56,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List<Recipe> recipes = [];
   List<Ingredient> ingredients = [];
   List<RecipeIngredient> kitchen = [];
@@ -65,17 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     this.fetchData();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   Future<void> fetchData() async {
@@ -96,8 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         this.kitchen.add(RecipeIngredient.fromJson(element));
       });
       setState(() {
-        _counter = this.recipes.length;
-        log(_counter.toString());
+
       });
     } else {
       throw Exception('Failed to load recipes');
@@ -106,53 +93,40 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: this.recipes.length,
+        itemBuilder: (context, index) {
+          return Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              height: 220,
+              width: double.maxFinite,
+              child: recipeCard(this.recipes[index])
+          );
+        }
+      )
+    );
+  }
+
+
+  Widget recipeCard(Recipe recipe) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Text(recipe.name),
+        decoration: new BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(recipe.imagePath),
+            //image: Image.network(recipe.imagePath),
+            fit: BoxFit.cover
+          )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
