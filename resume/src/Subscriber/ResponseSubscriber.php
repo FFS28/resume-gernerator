837e2d977ca6f252f814e95c1c4786df020f34ad
Jérémy ACHAIN <achain.jeremy@gmail.com>
2021-03-17 11:47:02 +0100

    Fix

--- resume/src/Subscriber/ResponseSubscriber.php
+++ resume/src/Subscriber/ResponseSubscriber.php
@@ -37,7 +37,6 @@ class ResponseSubscriber implements EventSubscriberInterface
         $response->headers->set("X-Frame-Options", 'deny');
         $response->headers->set("X-XSS-Protection", '1; mode=block');
         $response->headers->set("X-Content-Type-Options", 'nosniff');
-        $response->headers->set("Access-Control-Allow-Origin", '^https?://localhost:?[0-9]*$');
         $response->setVary('Accept-Encoding');
     }
 }
