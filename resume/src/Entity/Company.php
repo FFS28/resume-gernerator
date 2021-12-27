bd7802dda7537ed6eef320dbff396a0c74f6ad27
Jérémy ACHAIN <jeremy.achain.external@stormshield.eu>
2021-12-27 13:24:40 +0100

    Add company file

--- resume/src/Entity/Company.php
+++ resume/src/Entity/Company.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
@@ -120,6 +121,23 @@ class Company
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $service;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $filename;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * @Vich\UploadableField(mapping="statements", fileNameProperty="filename")
+     * @var File
+     */
+    private $file;
 
     public function __construct()
     {
@@ -506,5 +524,35 @@ class Company
         $this->reference = $reference;
 
         return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(?string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function setFile(File $file = null)
+    {
+        $this->file = $file;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($file) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getFile()
+    {
+        return $this->file;
     }
 }
